9706a50817882cbffe047c95221c0192087ff219
techagentng <techagentng@gmail.com>
2022-04-19 16:13:17 +0100

    mod commit

--- internals/core/ports/commerce.go
+++ internals/core/ports/commerce.go
@@ -1,11 +1,11 @@
 package ports
 
-import "ecommerce-boilerplate/internals/core/domain"
+import "ecommerce-boiler/internals/core/domain"
 // ICompanyService defines the interface for company service
 type ICommerceService interface {
-	GetCompanyByID(id string) (*domain.User, error)
-	GetAllCompany(pagination *utils.Pagination) (*utils.Pagination, error)
-	CreateCompany(company *domain.Company) error
-	UpdateCompany(params common.GetByIDRequest, company common.UpdateCompanyRequest) (*domain.Company, error)
+	GetUserByID(id string) (*domain.User, error)
+	GetAllUser(pagination *utils.Pagination) (*utils.Pagination, error)
+	CreateUser(company *domain.User) error
+	UpdateUser(params common.GetByIDRequest, company common.UpdateCompanyRequest) (*domain.User, error)
 	DeleteCompany(id string) error
 }
